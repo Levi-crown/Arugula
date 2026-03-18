b201cc312e90a2534e508e54e98e38abc2a74635
Levi-crown <hachicrown@gmail.com>
2026-03-18 21:21:39 +0800

    fix: main.tsx render

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import App from "./app/App"; // 🔥 IMPORTANT
 
-<BrowserRouter basename="/Arugula/">
-  <App />
-</BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/Arugula/">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
