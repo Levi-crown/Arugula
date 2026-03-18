47c602811be674862227034b468834c6dd771bd4
Levi-crown <hachicrown@gmail.com>
2026-03-18 21:08:31 +0800

    fix: routing for github pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,5 @@
+import { BrowserRouter } from "react-router-dom";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App";
-  import "./styles/index.css";
-
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+<BrowserRouter basename="/Arugula/">
+  <App />
+</BrowserRouter>
